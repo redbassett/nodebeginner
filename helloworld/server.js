345ec736f5cfa571eb7d3379c6dc7cac6dde856e
Harry Grillo <redbassett@gmail.com>
2013-12-15 20:34:37 -0500

    Converted completely to module

--- helloworld/server.js
+++ helloworld/server.js
@@ -1,11 +1,5 @@
 var http = require('http');
 
-/*http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
-}).listen(8888);*/
-
 function start() {
 	function onRequest(request, response) {
 		console.log('Request Received');
@@ -17,3 +11,5 @@ function start() {
 	http.createServer(onRequest).listen(8888);
 	console.log('Server has started.');
 }
+
+exports.start = start;
